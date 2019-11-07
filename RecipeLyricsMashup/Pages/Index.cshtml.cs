63ad0d0093c90749911c4735578f0e71ab358430
wressley <johnson.kyle.eric@gmail.com>
2019-11-06 21:30:09 -0500

    Add project files.

---
+++ RecipeLyricsMashup/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RecipeLyricsMashup.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string recipePuppyEndpoint = "http://www.recipepuppy.com/api/?q=jambalaya";
+                string recipePuppyJson = webClient.DownloadString(recipePuppyEndpoint);
+                //string accessToken = "7K8utXpOpn-DCw9kzjn7n7vB6Y7ss0a0RqfmT-03_yA2BabKbstji0cBOoT_dVvI";
+                //string geniusEndpoint = "https://api.genius.com/search?q=Fireball&access_token=" + accessToken;
+                //string geniusJson = webClient.DownloadString(geniusEndpoint);
+                //QuickType.SearchResult searchResults = QuickType.SearchResult.FromJson(geniusJson);
+                ////Plant[] allPlants = Plant.FromJson(plantJson);
+                //QuickType.Hit[] results = searchResults.Response.Hits;
+                //foreach(QuickType.Hit result in results)
+                //{
+                //    Console.WriteLine(result.Result.Title);
+                //    string lyricsEndpoint = "https://api.genius.com/songs/"+ result.Result.Id + "?text_format=plain&access_token=" + accessToken;
+                //    string lyricsJson = webClient.DownloadString(lyricsEndpoint);
+                //    Console.WriteLine(result.Result.PrimaryArtist.Name);
+                //}
+            }
+        }
+    }
+}

2254d1f89ddf811f344a13a3ba5328fe0b83ed61
liben37UC <pliben1994@gmail.com>
2019-12-02 18:21:05 -0500

    Added the extra json consumption from another group under the name ExtraJsonConsumption

---
+++ RecipesLyricsMashup/RecipesLyricsMashupV2/RecipeLyricsMashup/RecipeLyricsMashup/Pages/ExtraJsonConsumption.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecipeLyricsMashup.Pages
+{
+    public class ExtraJsonConsumptionModel : PageModel
+    {
+        public void OnGet()
+        {
+            String countryString = RetrieveData("https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json");
+            QuickTypeCountry.Welcome[] countries = QuickTypeCountry.Welcome.FromJson(countryString);
+            ViewData["Countries"] = countries;
+        }
+
+        public string RetrieveData(string endPoint)
+        {
+            string downloadedData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedData = webClient.DownloadString(endPoint);
+            }
+            return downloadedData;
+        }
+    }
+}
+
+
